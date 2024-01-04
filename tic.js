828f079863fc4b944c57b7baeff9cd25202dfa75
Abhilash-03-22 <mauryaabhi2003@gmail.com>
2024-01-04 18:49:40 +0530

    tried to make multiplater and ai modes but lack of time, so we are there without implemeting it

--- tic.js
+++ tic.js
@@ -7,6 +7,36 @@ let windialogue="<h1>CONGRATULATIONS \nWinner Is</h1>";
 let Div=document.createElement("div");
 let counter=0;
 let won=false;
+// let Toggle=document.getElementById("toggle");
+const aiPlayer = 'X';
+let AI=true;
+// Toggle.addEventListener("click",()=>{
+//     if(AI){
+//         AI=false;
+//     }
+//     else{
+//         AI=true;
+//     }
+//     selectmode();
+// })
+// Function to make the AI move
+const makeAIMove = () => {
+    // For simplicity, let's make a random move in an empty spot
+
+    // Filter out the empty spots
+    const emptySpots = Array.from(boxes).filter(box => box.innerText === '');
+
+    // Choose a random empty spot
+    const randomIndex = Math.floor(Math.random() * emptySpots.length);
+    const randomEmptyBox = emptySpots[randomIndex];
+
+    // Make the AI move
+    randomEmptyBox.innerText = aiPlayer;
+    randomEmptyBox.disabled = true;
+    turn0=true;
+    // Check for a winner after the AI move
+    checkwinner();
+};
 const winPatterns=[
     [0,1,2],
     [0,3,6],
@@ -17,30 +47,49 @@ const winPatterns=[
     [3,4,5],
     [6,7,8]
 ]
-boxes.forEach((box)=>{
-    box.addEventListener("click",()=>{
-        console.log("box was clicked");
-        if(turn0){
-            box.innerHTML="0";
-            turn0=false;
-        }
-        else{
-            box.innerHTML="X";
-            turn0=true;
-        }
-        box.disabled=true;
-        checkwinner();
+function modeAI(){
+    makeAIMove();
+    boxes.forEach((box)=>{
+        box.addEventListener("click",()=>{
+            console.log("box was clicked");
+            if(turn0){
+                box.innerHTML="0";
+                turn0=false;
+            }
+            box.disabled=true;
+             // AI move after player's move
+            makeAIMove();
+            checkwinner();  
+        })
     })
-})
+}
+// function modeMutiplayer(){
+//     boxes.forEach((box)=>{
+//         box.addEventListener("click",()=>{
+//             console.log("box was clicked");
+//             if(turn0){
+//                 box.innerHTML="0";
+//                 turn0=false;
+//             }
+//             else{
+//                 box.innerHTML="X";
+//                 turn0=true;
+//             }
+//             box.disabled=true;
+//             checkwinner();
+//         })
+//     })
+// }
 const Newgame=()=>{
     won=false;
     counter=0;
     newgame.style.display="none";
     Div.style.display="none";
     boxes.forEach((box)=>{
-        box.disabled=false;
-        box.innerText="";
+    box.disabled=false;
+    box.innerText="";
     })
+    makeAIMove();
 }
 newgame.addEventListener("click",()=>{
     Newgame();
@@ -70,7 +119,7 @@ const checkwinner=()=>{
         let posval1=boxes[pattern[0]].innerText;
         let posval2=boxes[pattern[1]].innerText;
         let posval3=boxes[pattern[2]].innerText;
-        if (posval1 !=""&& posval2 !="" && posval3!=""){
+        if (posval1 !==""&& posval2 !=="" && posval3!==""){
             if(posval1==posval2&&posval2==posval3){
                 winEvents();
                 Body.appendChild(Div).innerHTML=`<h1>CONGRATULATIONS \n</h1> <h2> ${posval1} Is Winner</h2>`;
@@ -78,9 +127,18 @@ const checkwinner=()=>{
                 Div.style.display="unset";
                 won=true;
                 boxes.forEach((box)=>{
-                box.disabled=true;
+                    box.disabled=true;
                 })
             }
         }
     }
 };
+// function selectmode(){
+// if(AI){
+//     modeAI();
+// }
+// else{
+//  modeMutiplayer();   
+// }
+// }
+modeAI();
